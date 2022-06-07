20b842ebcb0efe99a319bdfacd3e652994e39ea1
LeoHaas <leo@kauz.ch>
2022-06-07 14:19:24 +0200

    MVVM Testing

--- M226_M120_Projekt_SwissTransport/ViewModel/ConnectionsViewModel.cs
+++ M226_M120_Projekt_SwissTransport/ViewModel/ConnectionsViewModel.cs
@@ -8,8 +8,12 @@ namespace M226_M120_Projekt_SwissTransport.ViewModel
 {
     public class ConnectionsViewModel
     {
+        public string from;
+        public string to;
         public string time;
         public DateTime date;
+        public string From { get; set; }
+        public string To { get; set; }
         public string Time { get; set; }
         
         public DateTime Date{ get; set; }
