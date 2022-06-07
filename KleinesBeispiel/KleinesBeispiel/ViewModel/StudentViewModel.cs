2a096b0a6105f2f57fe7cefc457cf55248efb690
LeoHaas <leo@kauz.ch>
2022-06-07 08:56:25 +0200

    KleinesBeispiel

---
+++ KleinesBeispiel/KleinesBeispiel/ViewModel/StudentViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using KleinesBeispiel.Model;
+
+namespace KleinesBeispiel.ViewModel
+{
+    class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Leo", LastName = "Haas" });
+            students.Add(new Student { FirstName = "Noah", LastName = "Zemljic" });
+            students.Add(new Student { FirstName = "Noah", LastName = "Horlacher" });
+            students.Add(new Student { FirstName = "David", LastName = "Peric" });
+
+            Students = students;
+        }
+    }
+}
